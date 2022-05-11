c77e534551c06855e5af2c17ae0d6829af69566b
Mohammad majed alshish <eng.mohammed.majed@gmail.com>
2022-05-11 18:53:50 +0300

    hope you like it

---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+function Header (props)
+{
+    return (
+        <>
+        <h1 id='head'>Welcome to a world full of wonders</h1>
+        </>
+    );
+}
+export default Header ;
